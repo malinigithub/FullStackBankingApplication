a498772eeba9f414a5e31002705f89c5aa2cd8d0
malinigithub <maliniradha2020@gmail.com>
2023-03-01 17:13:07 -0500

    removing consolelog for URL

--- dal.js
+++ dal.js
@@ -4,7 +4,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 const url = process.env.DB_URL;
 let db = null;
-console.log("URL" + process.env.DB_URL);
 
 // connect to mongo
 MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
