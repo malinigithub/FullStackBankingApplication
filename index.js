1f7b185d88f83ee1b0f8348d80f0a89339521126
malinigithub <maliniradha2020@gmail.com>
2023-03-02 18:20:35 -0500

    fixed user token issues

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ app.use(bodyParser.json());
 
 const authenticateJWT = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  console.log(`Headers: ${JSON.stringify(req.headers)}`);
-  console.log(`Body: ${JSON.stringify(req.body)}`);
+  //console.log(`Headers: ${JSON.stringify(req.headers)}`);
+  //console.log(`Body: ${JSON.stringify(req.body)}`);
   if (authHeader) {
     const token = authHeader.split(" ")[1];
 
@@ -29,7 +29,6 @@ const authenticateJWT = (req, res, next) => {
       if (err) {
         return res.sendStatus(403);
       }
-
       req.user = user;
       next();
     });
@@ -64,25 +63,20 @@ app.get("/account/create/:name/:email/:password", function (req, res) {
         .then((user) => {
           console.log(user);
           const accessToken = jwt.sign(
-            { username: user.username, role: user.role },
+            { useremail: req.params.email, role: user.role },
             accessTokenSecret,
             { expiresIn: "20m" }
           );
           const refreshToken = jwt.sign(
-            { username: user.username, role: user.role },
+            { useremail: req.params.email, role: user.role },
             refreshTokenSecret
           );
-
           refreshTokens.push(refreshToken);
-          //token = "bearer_token=" + accessToken;
-          //document.cookie = token;
-          //document.co
           res.json({
             accessToken,
             refreshToken,
             user,
           });
-          //res.send(user);
         });
     }
   });
@@ -111,26 +105,22 @@ app.get("/account/login/:email/:password", function (req, res) {
     if (user.length > 0) {
       if (user[0].password === req.params.password) {
         const accessToken = jwt.sign(
-          { username: user.username, role: user.role },
+          { useremail: req.params.email, role: user.role },
           accessTokenSecret,
           { expiresIn: "20m" }
         );
         const refreshToken = jwt.sign(
-          { username: user.username, role: user.role },
+          { useremail: req.params.email, role: user.role },
           refreshTokenSecret
         );
 
         refreshTokens.push(refreshToken);
-        //token = "bearer_token=" + accessToken;
-        //document.cookie = token;
-        //document.co
-        currentUser = user[0];
+
         res.json({
           accessToken,
           refreshToken,
           user,
         });
-        //res.send(user[0]);
       } else {
         res.send("Login failed: wrong password");
       }
@@ -141,28 +131,46 @@ app.get("/account/login/:email/:password", function (req, res) {
 });
 // find user account
 app.get("/account/find/:email", authenticateJWT, function (req, res) {
-  dal.find(req.params.email).then((user) => {
-    console.log(user);
-    res.send(user);
-  });
+  const { useremail, role } = req.user;
+  if (useremail === req.params.email) {
+    dal.find(req.params.email).then((user) => {
+      console.log(user);
+      res.send(user);
+    });
+  } else {
+    res.sendStatus(403);
+  }
 });
 
 // find one user by email - alternative to find
 app.get("/account/findOne/:email", authenticateJWT, function (req, res) {
-  dal.findOne(req.params.email).then((user) => {
-    console.log(user);
-    res.send(user);
-  });
+  const { useremail, role } = req.user;
+
+  if (useremail === req.params.email) {
+    dal.findOne(req.params.email).then((user) => {
+      console.log(user);
+      res.send(user);
+    });
+  } else {
+    res.sendStatus(403);
+  }
 });
 
 // update - deposit/withdraw amount
 app.get("/account/update/:email/:amount", authenticateJWT, function (req, res) {
-  var amount = Number(parseFloat(req.params.amount).toFixed(2));
+  const { useremail, role } = req.user;
+  //console.log("user", useremail);
 
-  dal.update(req.params.email, amount).then((response) => {
-    console.log(response);
-    res.send(response);
-  });
+  if (useremail === req.params.email) {
+    var amount = Number(parseFloat(req.params.amount).toFixed(2));
+
+    dal.update(req.params.email, amount).then((response) => {
+      console.log(response);
+      res.send(response);
+    });
+  } else {
+    res.sendStatus(403);
+  }
 });
 
 // all accounts
