c626591c01616e73633306e0a4d517c7894ff375
malinigithub <maliniradha2020@gmail.com>
2023-03-07 16:12:57 -0500

    Name updated

--- public/home.js
+++ public/home.js
@@ -3,7 +3,7 @@ function Home() {
     <Card
       bgcolor="info"
       txtcolor="white"
-      header="BadBank Home Page"
+      header="RadBank Home Page"
       title="Welcome to the bank"
       text="You can move around using the navigation bar."
       body={<img src="bank.png" className="img-fluid" alt="Responsive image" />}
